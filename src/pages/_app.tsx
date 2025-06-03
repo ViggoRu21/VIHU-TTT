5589e6db752ef943f70490c8bd592fe9201e5892
Glasabarn <viggoh21@ru.is>
2025-06-03 20:58:01 +0000

    Replace Moment with Date-fns

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,20 +9,20 @@ console.log(process.env.NEXT_PUBLIC_DatadogAppId);
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-datadogRum.init({
-    applicationId: process.env.NEXT_PUBLIC_DATADOG_APPLICATION_ID!,
-    clientToken: process.env.NEXT_PUBLIC_DATADOG_APPLICATION_ID!,
-    // `site` refers to the Datadog site parameter of your organization
-    // see https://docs.datadoghq.com/getting_started/site/
-    site: 'datadoghq.eu',
-    service: 'vihu-ttt',
-    env: process.env.NEXT_PUBLIC_NODE_ENV!,
-    // Specify a version number to identify the deployed version of your application in Datadog
-    // version: '1.0.0',
-    sessionSampleRate: 100,
-    sessionReplaySampleRate: 20,
-    defaultPrivacyLevel: 'mask-user-input',
-});
+// datadogRum.init({
+//     applicationId: process.env.NEXT_PUBLIC_DATADOG_APPLICATION_ID!,
+//     clientToken: process.env.NEXT_PUBLIC_DATADOG_APPLICATION_ID!,
+//     // `site` refers to the Datadog site parameter of your organization
+//     // see https://docs.datadoghq.com/getting_started/site/
+//     site: 'datadoghq.eu',
+//     service: 'vihu-ttt',
+//     env: process.env.NEXT_PUBLIC_NODE_ENV!,
+//     // Specify a version number to identify the deployed version of your application in Datadog
+//     // version: '1.0.0',
+//     sessionSampleRate: 100,
+//     sessionReplaySampleRate: 20,
+//     defaultPrivacyLevel: 'mask-user-input',
+// });
 
   return (
     <>
